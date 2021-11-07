55956330128b687aa3c1b477ad19fe0a9143a12f
Carlos <carlosporrasayala@gmail.com>
2021-11-07 08:17:36 -0500

    Agregar archivos de proyecto.

---
+++ FBTarjeta/Migrations/20211101113510_v1.0.1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FBTarjeta.Migrations
+{
+    public partial class v101 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NumeroTarjeta",
+                table: "TarjetaCredito",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumeroTarjeta",
+                table: "TarjetaCredito");
+        }
+    }
+}
